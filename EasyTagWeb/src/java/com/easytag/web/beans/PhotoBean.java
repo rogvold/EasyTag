67e30e1e2a4e322dfdbedde49871e0bc47d6a61a
vima <vit312@gmail.com>
2013-11-29 05:37:05 +0300

    row

--- EasyTagWeb/src/java/com/easytag/web/beans/PhotoBean.java
+++ EasyTagWeb/src/java/com/easytag/web/beans/PhotoBean.java
@@ -3,6 +3,7 @@ package com.easytag.web.beans;
 import com.easytag.core.entity.jpa.Photo;
 import com.easytag.core.managers.PhotoManagerLocal;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
@@ -17,9 +18,10 @@ public class PhotoBean implements Serializable {
     
     private Long photoId;
     private Long prevId;
-    private Long nextId;
+    private Long nextId;    
     private Photo photo;
     private List<Photo> albPh;
+    private List<Photo> row;
     
     
     public Photo getPhoto() {
@@ -46,15 +48,22 @@ public class PhotoBean implements Serializable {
 
     public void setPhotoId(String photoId) {
         System.out.println("setPhotoId: " + photoId);
-        this.photoId = Long.parseLong(photoId);
+        this.photoId = Long.parseLong(photoId);        
         this.photo = photoMan.getPhotoById(this.photoId);
         this.albPh = photoMan.getPhotosInAlbum(photo.getAlbumId());        
-        int curr = albPh.indexOf(photo);
-        System.out.println("Current phIndex: " + curr);
-        int next = (curr + 1 == albPh.size())? 0: (curr + 1);
+        
+        int curr = albPh.indexOf(photo);  //System.out.println("Current phIndex: " + curr);                     
+        int next = (curr + 1 == albPh.size())? 0: (curr + 1);        
         int prev = (curr == 0)? (albPh.size() - 1): (curr - 1);
+        int afterNext = (next + 1 == albPh.size())? 0: (next + 1);
         this.nextId = albPh.get(next).getId(); 
         this.prevId = albPh.get(prev).getId();
+        
+        row = new ArrayList<Photo>(4);
+        row.add(albPh.get(prev));
+        row.add(albPh.get(curr));
+        row.add(albPh.get(next));
+        row.add(albPh.get(afterNext));        
     }
 
     public Long getPrevId() {
@@ -73,5 +82,11 @@ public class PhotoBean implements Serializable {
         this.nextId = nextId;
     }
     
+    public Long getPreviewId(int ind) {
+        return row.get(ind).getPreviewId();
+    }   
     
+    public Long getPhId(int ind) {
+        return row.get(ind).getId();
+    }   
 }
